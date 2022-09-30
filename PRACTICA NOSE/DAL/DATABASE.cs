1d2e85c0b8b1f1d6b470f0e4452c863f04ff2d49
Alumnos <Alumnos@LABC2PC10>
2022-09-30 12:07:52 -0600

    Agregar archivos de proyecto.

---
+++ PRACTICA NOSE/DAL/DATABASE.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PRACTICA_NOSE.Properties;
+using System.Configuration;
+
+namespace PRACTICA_NOSE.DAL
+{
+    public class DATABASE
+    {public static string getStrConnection()
+        {
+            return Settings.Default.empleadosConnectionString;
+        }
+        public SqlConnection getConnection()
+        {
+             SqlConnection Con = new SqlConnection(getStrConnection());
+            return Con;
+
+        }
+
+
+    }
+}
